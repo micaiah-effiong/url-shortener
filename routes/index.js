ad799be5ad19de98cec9abdc5bcc11fe63f6ee1a
micaiah-effiong <micaiah.effiong@gmail.com>
2020-08-05 15:42:41 +0100

    created routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+var links = require("./links");
 var router = express.Router();
 
+router.use("/link", links);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
--- routes/links.js
+++ routes/links.js
@@ -1,9 +1,38 @@
 var express = require("express");
+var handleAsync = require("../handles/async-handler");
+var { link } = require("../models/index");
 var router = express.Router();
+
+router.use((req, res, next) => {
+  req.fullPath = `${req.protocol}://${req.hostname}:${req.app.settings.port}${req.originalUrl}`;
+  next();
+});
 
 /* GET links listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+router.get(
+  "/:link",
+  handleAsync(async function (req, res, next) {
+    let originalUrl = await link.findOne({ where: { slug: req.params.link } });
+    if (!originalUrl) return next();
+    res.redirect(`http://${originalUrl.url}`);
+  })
+);
+
+router.post(
+  "/",
+  handleAsync(async (req, res, next) => {
+    console.log(req.body);
+    let { slug, url } = req.body;
+    let createdLink = await link.create({ slug, url });
+    res.status(201).json({
+      success: true,
+      data: createdLink,
+    });
+  })
+);
 
 module.exports = router;
