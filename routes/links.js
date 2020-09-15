7e26cd5341ef14c95432bd89b297dd0708875f35
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-09-15 12:14:19 +0100

    code refactor - KISS and clean

--- routes/links.js
+++ routes/links.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { nanoid } = require("nanoid");
-const handleAsync = require("../handlers/async-handler");
-const { link } = require("../models/index");
+const {
+  create,
+  getAll,
+  getOne,
+  deleteOne,
+} = require("../controllers/index").links;
 const router = express.Router();
 const url = "";
+
+// middleware
 router.use((req, res, next) => {
   req.fullPath =
     url ||
@@ -12,65 +18,7 @@ router.use((req, res, next) => {
 });
 
 /* GET links listing. */
-router.get(
-  "/",
-  handleAsync(async (req, res, next) => {
-    let data = await link.find();
-    res.json({
-      success: true,
-      data,
-    });
-  })
-);
-
-router.get(
-  "/:slug",
-  handleAsync(async (req, res, next) => {
-    let data = await link.findOne({ slug: req.params.slug });
-    res.json({
-      success: true,
-      data,
-    });
-  })
-);
-
-router.post(
-  "/",
-  handleAsync(async (req, res, next) => {
-    let { slug, url, expiresAt } = req.body;
-
-    // set expire time is not assigned
-    expiresAt = expiresAt || new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
-
-    if (slug) {
-      // check is slug is valid
-      if (/[^\w\-]/i.test(slug)) return next(Error("Invalid slug"));
-    } else {
-      slug = nanoid(7); // create slug with nanoid
-    }
-
-    let _modelLink = new link({ slug, url, expiresAt });
-    let createdLink = await _modelLink.save();
-    // let newLinkObj = { ...createdLink._doc };
-    data = req.fullPath + createdLink.slug;
-
-    res.status(201).json({
-      success: true,
-      data,
-    });
-  })
-);
-
-router.delete(
-  "/:slug",
-  handleAsync(async (req, res, next) => {
-    let data = await link.findOneAndDelete({ slug: req.params.slug });
-    console.log(data);
-    res.json({
-      success: true,
-      data,
-    });
-  })
-);
+router.route("/").get(getAll).post(create);
+router.route("/:slug").get(getOne).delete(deleteOne);
 
 module.exports = router;
