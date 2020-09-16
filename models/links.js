8d3ca3852e64a9b8dfcaa68b1879e1058a119bf7
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-09-16 23:05:17 +0100

    created a visit schema to get details of each click, also added timestamps to links

--- models/links.js
+++ models/links.js
@@ -1,4 +1,10 @@
-const schema = {
+const visitSchema = {
+  referer: String,
+  ipAddress: String,
+  visitAt: { type: Date, required: true, default: new Date() },
+};
+
+const linkSchema = {
   url: {
     type: String,
     required: [true, "a valid url is required"],
@@ -29,18 +35,31 @@ const schema = {
       message: (prop) => `${prop.value} is invalid`,
     },
   },
+
   expiresAt: {
     type: Date,
     required: [true, "a slug is required"],
     default: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
   },
+
   clicks: {
     type: Number,
     default: 0,
   },
+
+  visit: {
+    type: [visitSchema],
+    index: true,
+  },
 };
 
 module.exports = function link(mongoose) {
-  let _schema = new mongoose.Schema(schema);
+  const option = {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  };
+  let _schema = new mongoose.Schema(linkSchema, option);
   return mongoose.model("link", _schema);
 };
