07f31493c6e5dac0e4d1f2c9b091c9d0ded6833f
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-09-17 18:58:18 +0100

    added and configured admin-bro for cpanel and dashboard

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const expressSession = require("express-session");
+const AdminBro = require("admin-bro");
+const AdminBroMongoose = require("@admin-bro/mongoose");
+const AdminBroExpress = require("@admin-bro/express");
 
 var indexRouter = require("./routes/index");
+const { link } = require("./models/index");
 
+/*admin-bro setup*/
+AdminBro.registerAdapter(AdminBroMongoose);
 var app = express();
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: "/admin",
+  resources: [link],
+});
+
+const router = AdminBroExpress.buildRouter(adminBro);
+app.use(adminBro.options.rootPath, router);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,14 +36,13 @@ app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.status(404).send("resoures not found");
+  res.status(404).send("resources not found");
 });
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.statusCode || 500);
