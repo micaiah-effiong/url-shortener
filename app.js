abfc5d4bcee2324bd52d35e115838708b4ad861b
Sreejith V S <sreejithvs333@gmail.com>
2020-10-04 17:05:49 +0530

    add a static middleware to serve static files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
