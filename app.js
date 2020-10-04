bbceeed6c09ccc2e9862c0797cc3bb94540c60a0
Sreejith V S <sreejithvs333@gmail.com>
2020-10-04 13:59:51 +0530

    remove static file serving in app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static('public'))
 
 app.use("/", indexRouter);
 
@@ -35,8 +34,5 @@ app.use(function (err, req, res, next) {
   res.status(err.statusCode || 500);
   res.json({ success: false, err, msg: err.message });
 });
-
-//Serving static files
-app.use('/static', express.static('public'))
 
 module.exports = app;
