228d5f9ef2e4622860d2660dc7cb955ed6fe5798
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-06 00:01:08 +0100

    refactor model to use subdocs

--- models/links.js
+++ models/links.js
@@ -1,58 +1,3 @@
-const visitSchema = {
-  referer: String,
-  ipAddress: String,
-  visitAt: { type: Date, required: true, default: new Date() },
-};
-
-const linkSchema = {
-  url: {
-    type: String,
-    required: [true, "a valid url is required"],
-    validate: {
-      validator: (_url) => {
-        let isValid;
-        try {
-          new URL(_url);
-          isValid = true;
-        } catch (err) {
-          isValid = false;
-        }
-        return isValid;
-      },
-      message: (prop) => `${prop.value} is an invalid URL`,
-    },
-  },
-
-  slug: {
-    type: String,
-    required: [true, "a slug is required"],
-    unique: [true, "slug already used"],
-    index: true,
-    validate: {
-      validator: (_slug) => {
-        return !/[^\w\-]/i.test(_slug);
-      },
-      message: (prop) => `${prop.value} is invalid`,
-    },
-  },
-
-  expiresAt: {
-    type: Date,
-    required: [true, "a slug is required"],
-    default: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-  },
-
-  clicks: {
-    type: Number,
-    default: 0,
-  },
-
-  visit: {
-    type: [visitSchema],
-    index: true,
-  },
-};
-
 module.exports = function link(mongoose) {
   const option = {
     timestamps: {
@@ -60,6 +5,67 @@ module.exports = function link(mongoose) {
       updatedAt: "updated_at",
     },
   };
-  let _schema = new mongoose.Schema(linkSchema, option);
-  return mongoose.model("link", _schema);
+
+  const _linkSchema = {
+    url: {
+      type: String,
+      required: [true, "a valid url is required"],
+      validate: {
+        validator: (_url) => {
+          let isValid;
+          try {
+            new URL(_url);
+            isValid = true;
+          } catch (err) {
+            isValid = false;
+          }
+          return isValid;
+        },
+        message: (prop) => `${prop.value} is an invalid URL`,
+      },
+    },
+
+    slug: {
+      type: String,
+      required: [true, "a slug is required"],
+      unique: [true, "slug already used"],
+      index: true,
+      validate: {
+        validator: (_slug) => {
+          return !/[^\w\-]/i.test(_slug);
+        },
+        message: (prop) => `${prop.value} is invalid`,
+      },
+    },
+
+    expiresAt: {
+      type: Date,
+      required: [true, "a slug is required"],
+      default: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    },
+
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+
+    visit: [
+      new mongoose.Schema(
+        {
+          referer: String,
+          ipAddress: String,
+          visitAt: { type: Date, required: true, default: new Date() },
+        },
+        {
+          timestamps: {
+            createdAt: "created_at",
+            updatedAt: null,
+          },
+        }
+      ),
+    ],
+  };
+
+  const linkSchema = new mongoose.Schema(_linkSchema, option);
+  return mongoose.model("link", linkSchema);
 };
