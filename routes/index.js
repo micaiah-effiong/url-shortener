c70c3b9a7cd0ff1643e08b9815ce9c4f7a5f8bb3
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-06 00:06:47 +0100

    code refactor - move code to controller to keep the route simple

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,12 @@
 const express = require("express");
 const links = require("./links");
-const { link } = require("../models/index");
 const handleAsync = require("../handlers/async-handler");
 const router = express.Router();
+const { visit } = require("../controllers/index").links;
 
 router.use("/link", links);
 
 /* GET home page. */
-router.get(
-  "/:link",
-  handleAsync(async function (req, res, next) {
-    let {
-      headers: { referer },
-      connection: { remoteAddress: ipAddress },
-      params: { link: slug },
-    } = req;
-    const originalUrl = await link.findOne({ slug });
-    const hasExpired = Date.now() > new Date(originalUrl.expiresAt).getTime();
-    if (!originalUrl) return next();
-    if (hasExpired) {
-      return next(Error("slug has expire"));
-    }
-    originalUrl.clicks++;
-    originalUrl.visit.push({
-      referer,
-      ipAddress,
-    });
-    await originalUrl.save();
-    res.redirect(originalUrl.url);
-  })
-);
-
-// TODO:
-// get CLICK TIME AND DATE
-// get click referer
+router.get("/:link", visit);
 
 module.exports = router;
