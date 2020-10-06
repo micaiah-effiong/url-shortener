669a7487d4031cd653efa27757d13b3adcf29b62
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-06 23:23:39 +0100

    created user controller

---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const { nanoid } = require("nanoid");
+const { handleAsync, errorResponse } = require("../handlers/index");
+const { user } = require("../models/index");
+
+const getOne = handleAsync(async (req, res, next) => {
+  const data = await user.findOne({ _id: req.params.id });
+  if (!data) return next(errorResponse("BAD REQUEST", 400));
+  return res.json({
+    data: "Success!",
+  });
+});
+
+const getAll = handleAsync(async (req, res, next) => {
+  let data = await user.find();
+  return res.json({
+    data,
+  });
+});
+
+const create = handleAsync(async (req, res, next) => {
+  const data = await user.create(req.body);
+  return res.json({
+    data,
+  });
+});
+
+const update = handleAsync(async (req, res, next) => {
+  const data = await user.findByIdAndUpdate(req.params.id, req.body);
+  return res.json({
+    data,
+  });
+});
+
+const deleteOne = handleAsync(async (req, res, next) => {
+  const data = await user.findByIdAndDelete(req.params.id);
+  return res.json({
+    data,
+  });
+});
+
+module.exports = {
+  _name: "users",
+  getOne,
+  getAll,
+  create,
+  update,
+  deleteOne,
+};
