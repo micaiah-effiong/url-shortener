7341052c56c458d244d0dfa08c63874f910868ba
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-07 10:06:14 +0100

    used users route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const links = require("./links");
+const users = require("./users");
 const handleAsync = require("../handlers/async-handler");
 const router = express.Router();
 const { visit } = require("../controllers/index").links;
 
 router.use("/link", links);
+router.use("/users", users);
 
 /* GET home page. */
 router.get("/:link", visit);
