d1eee0df72c029ad789a3997ba926b2b336b00a7
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-08 11:09:58 +0100

    set up auth handler

---
+++ handlers/auth.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const Local = require("passport-local").Strategy;
+const { user: Users } = require("../models");
+
+passport.use(
+  new Local(
+    {
+      usernameField: "email",
+    },
+    async function (email, password, done) {
+      try {
+        let user = await Users.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "Incorrect email or password" });
+        }
+        if (!(await user.verifyPassword(password))) {
+          return done(null, false, { message: "Incorrect email or password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        throw err;
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    let user = await Users.findById(id);
+    if (!user) throw Error("UNAUTHORIZED");
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = { name: "auth", passport };
