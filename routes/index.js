9e3df823420d9693939a59dbacf9c20bc9664d1e
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-08 11:09:38 +0100

    setup auth route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const links = require("./links");
 const users = require("./users");
-const handleAsync = require("../handlers/async-handler");
+const auth = require("./auth");
 const router = express.Router();
 const { visit } = require("../controllers/index").links;
 
 router.use("/link", links);
 router.use("/users", users);
+router.use("/auth", auth);
 
 /* GET home page. */
 router.get("/:link", visit);
