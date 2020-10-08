0c0d41121107fa0c2f9be8e7737e6db7f16ced8a
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-08 23:38:12 +0100

    code refactor

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const expressSession = require("express-session");
 const FileStore = require("session-file-store")(expressSession);
-const { passport } = require("./handlers/index").auth;
-
-var indexRouter = require("./routes/index");
-const { link } = require("./models/index");
+const { passport } = require("./handlers/index");
+const indexRouter = require("./routes/index");
 
 const app = express();
 const fileStoreOptions = {};
@@ -19,18 +17,18 @@ app.set("view engine", "ejs");
 // middlewares
 app.use(logger("dev"));
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-let sessOption = {
+const sessOption = {
   secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
+  resave: false,
+  saveUninitialized: false,
   store: new FileStore(fileStoreOptions),
 };
 app.use(expressSession(sessOption)); //will be set with secret form env
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // main route
 app.use("/", indexRouter);
@@ -44,10 +42,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  console.log({ err, msg: err.message, r: err.stack });
 
   // render the error page
   res.status(err.statusCode || 500);
-  res.json({ err, msg: err.message });
+  res.json({ err, msg: err.message, r: err.stack });
 });
 
 module.exports = app;
