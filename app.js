6a2e2833b59d90eedb9892599e65cec0bf6575a6
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-08 11:08:23 +0100

    configure session-file-store and passportjs in app

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const expressSession = require("express-session");
+const FileStore = require("session-file-store")(expressSession);
+const { passport } = require("./handlers/index").auth;
 
 var indexRouter = require("./routes/index");
 const { link } = require("./models/index");
 
-var app = express();
+const app = express();
+const fileStoreOptions = {};
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// middlewares
 app.use(logger("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+let sessOption = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new FileStore(fileStoreOptions),
+};
+app.use(expressSession(sessOption)); //will be set with secret form env
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
+
+// main route
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
