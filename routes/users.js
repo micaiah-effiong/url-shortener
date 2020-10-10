971cd0b6d4e51563bc6038b9ea0502c0bb19a7eb
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-10 20:25:19 +0100

    added: authenticate and login user after registration

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,10 @@ const {
   update,
 } = require("../controllers/index").users;
 const { isAuth } = require("../middlewares/index");
+const { login } = require("../controllers/index").auth;
 const router = express.Router();
 
-router.route("/").get(isAuth, getAll).post(create);
+router.route("/").get(isAuth, getAll).post(create, login);
 router.use(isAuth);
 router.route("/:id").get(getOne).put(update).delete(deleteOne);
 
