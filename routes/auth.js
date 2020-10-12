7b113f68e3ed2d9d018d0fcc9be3b46b1340e6e8
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-12 17:25:32 +0100

    added endpoints for password reset and request password reset

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { passport, handleAsync } = require("../handlers/index");
-const { login, logout } = require("../controllers/index").auth;
+const {
+  login,
+  logout,
+  resetPassword,
+  reqPasswordReset,
+} = require("../controllers/index").auth;
 const router = express.Router();
 
 router.post("/login", login);
-
+router.post("/reset-password/:token", resetPassword);
+router.post("/request-password-reset", reqPasswordReset);
 router.get("/logout", logout);
 
 module.exports = router;
