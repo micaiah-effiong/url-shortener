77b931b8959464382f5c5c24ebecebf9949786d9
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-14 16:59:36 +0100

    refactor error handling, to proper identify error

--- handlers/access-control.js
+++ handlers/access-control.js
@@ -1,17 +1,11 @@
 module.exports = function accessControl(...data) {
-  if (!(data || Array.isArray(data))) throw Error("data must be an Array");
+  if (!data) throw Error("data can not be null or undefined");
+
+  if (!Array.isArray(data)) throw Error("data must be an Array");
 
   return (req, res, next) => {
-    console.log(
-      data,
-      data.includes(req.user.role),
-      req.user.role,
-      req.user,
-      !(req.user && data.includes(req.user.role))
-    );
     if (!(req.user && data.includes(req.user.role)))
       return next(Error("UNAUTHORIZED"));
-
     return next();
   };
 };
