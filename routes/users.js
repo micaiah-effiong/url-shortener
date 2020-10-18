81c70e0e4a658e207143374e72edc62188c69584
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-18 23:01:11 +0100

    added endpoints for sharing and accepting links invitaions

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const {
   update,
   profile,
   userLinks,
+  shareLinkWithUsers,
+  acceptLinkInvite,
 } = require("../controllers/index").users;
 const { isAuth } = require("../middlewares/index");
 const { login } = require("../controllers/index").auth;
@@ -16,6 +18,8 @@ router.route("/").get(isAuth, getAll).post(create, login);
 router.use(isAuth);
 router.route("/me").get(profile);
 router.route("/links").get(userLinks);
+router.route("/links/share").post(shareLinkWithUsers);
+router.route("/links/accept").get(acceptLinkInvite);
 router.route("/:id").get(getOne).put(update).delete(deleteOne);
 
 module.exports = router;
