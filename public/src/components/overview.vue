23256e01881f73b56ec183778f64fd8d45273f58
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-10-25 00:58:32 +0100

    added statistics card

--- public/src/components/overview.vue
+++ public/src/components/overview.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Overview Page</div>
+  <div>
+    <div>Overview Page</div>
+    <StatisticsCard :figure="200" text="Clicks" />
+    <StatisticsCard :figure="30" text="Total Links" />
+    <StatisticsCard :figure="60" text="Something else" />
+    <StatisticsCard :figure="10" text="Small one" />
+  </div>
 </template>
 
 <script>
+import StatisticsCard from "./statistics-card.vue";
 export default {
   name: "Overview",
+  components: { StatisticsCard },
 };
 </script>
---
+++ public/src/components/statistics-card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="group-card">
+    <div class="card-fig">
+      {{ figure }}
+    </div>
+    <div class="card-text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Statistics-Card",
+  props: {
+    figure: Number,
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.group-card {
+  width: 29%;
+  background-color: #70bcf9;
+  border-radius: 5px;
+  box-shadow: 0 0 11px #a8a3a3;
+  padding: 10px 20px;
+  margin: 13px 0;
+}
+
+.card-fig {
+  font-size: 3em;
+}
+
+.card-text {
+  font-size: 20px;
+}
+</style>
