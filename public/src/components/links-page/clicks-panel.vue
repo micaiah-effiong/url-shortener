d9c3912570e7890eeb720047e9e05920a8490414
Micaiah Effiong <micaiah.effiong@gmail.com>
2020-11-03 13:40:11 +0100

    use line graph for visualising clicks

--- public/src/components/links-page/clicks-panel.vue
+++ public/src/components/links-page/clicks-panel.vue
@@ -1,32 +1,58 @@
 <template>
   <div class="panel p-3">
     <div>🚀Clicks</div>
-    <PieChart :chartdata="dataCollection" :options="chartOptions" />
-    <div>Info here</div>
+    <div class="flex justify-evenly">
+      <LineChart
+        class="p-3 ml-3 shadow"
+        style="width: 60%; background-color: #fff; border-radius: 6px;"
+        :chartdata="dataCollection"
+        :options="chartOptions"
+      />
+      <div
+        style="width: 30%; background-color: #3949AB; color: white; border-radius: 6px;"
+        class="shadow p-3"
+      >
+        Today: 3 Last 7 days: 8 Last 30 days: 20 #3949AB
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import PieChart from "../chart/pie.js";
+import LineChart from "../chart/line.js";
 export default {
   name: "ClicksPanel",
-  components: { PieChart },
+  components: { LineChart },
   data() {
     return {
       chartOptions: {
         responsive: true,
         maintainAspectRatio: false,
         legend: {
-          position: "bottom",
+          // position: "top",
+          display: false,
         },
-        cutoutPercentage: 55,
+        scales: {
+          xAxes: [
+            {
+              gridLines: { display: false },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: { display: true },
+            },
+          ],
+        },
       },
       dataCollection: {
-        labels: ["1", "2", "3"],
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
         datasets: [
           {
-            data: [70, 50, 60],
-            backgroundColor: ["#0D47A1", "#1976D2", "#2196F3"],
+            label: "7 days",
+            borderColor: "#673AB7",
+            data: [10, 2, 8, 12, 5, 7, 3, 19, 10],
+            backgroundColor: "#fff0",
           },
         ],
       },
