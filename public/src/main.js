45ef09bda0832f706bb5ca015019c1324f163b57
Emilsone <lawsonezekiel68@gmail.com>
2020-11-11 20:51:43 +0100

    create location card

--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,8 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-// import '../assets/css/tailwind.css';
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+
+import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false;
 
